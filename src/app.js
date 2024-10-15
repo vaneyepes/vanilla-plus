6b14983a0f05281af3cf9c8eddeb398771fefd47
vaneyepes <48944459+vaneyepes@users.noreply.github.com>
2024-10-15 18:59:49 +0200

    Solved bugs in description and humidity Element

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ function showTemp(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 }
 
 function search(event) {
